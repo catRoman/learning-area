99dbf7f0ddcceeb599b2d7c975a32a4d5511fbbc
catRoman <cjroman@lakeheadu.ca>
2020-09-04 22:10:48 -0700

    external example complete

--- javascript/introduction-to-js-1/what-is-js/script.js
+++ javascript/introduction-to-js-1/what-is-js/script.js
@@ -1,11 +1,12 @@
 function createParagraph() {
-  let para = document.createElement('p');
-  para.textContent = 'You clicked the button!';
-  document.body.appendChild(para);
+    let para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+
 }
 
 const buttons = document.querySelectorAll('button');
 
-for(let i = 0; i < buttons.length ; i++) {
-  buttons[i].addEventListener('click', createParagraph);
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', createParagraph);
 }
